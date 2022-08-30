dcf97cb07a1bbdbd8647323279436c655ec5e7ed
Ivan hanák <hanakivan@gmail.com>
2022-08-30 09:36:08 +0200

    added plugin

---
+++ settings-page.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Special thanks to: https://deliciousbrains.com/create-wordpress-plugin-settings-page/
+ */
+
+add_action( 'admin_menu', function (): void {
+	add_options_page(
+		TBE_PLUGIN_NAME,
+		TBE_PLUGIN_NAME,
+		'manage_options',
+		TBE_SLUG,
+		'tbe_render_plugin_settings_page'
+	);
+} );
+
+function tbe_render_plugin_settings_page(): void {
+	?>
+	<h2><?php echo TBE_PLUGIN_NAME;?></h2>
+	<form action="options.php" method="post">
+        <?php
+        settings_fields( TBE_OPTIONS_NAME );
+		do_settings_sections( 'lrfw_plugin' ); ?>
+		<input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
+	</form>
+	<?php
+}
+
+add_action( 'admin_init', function () {
+	register_setting( TBE_OPTIONS_NAME, TBE_OPTIONS_NAME, 'lrfw_example_plugin_options_validate' );
+	add_settings_section( 'api_settings', 'Translations', 'lrfw_plugin_section_text', 'lrfw_plugin' );
+} );
+
+function lrfw_example_plugin_options_validate( $input ) {
+    if(isset($_POST['translations']) && is_array($_POST['translations'])) {
+        $contents = json_encode($_POST["translations"], JSON_PRETTY_PRINT);
+
+        file_put_contents(TBE_PLUGIN_DIR."/data.json", $contents);
+
+	    return $_POST["translations"];
+    };
+}
+
+function lrfw_plugin_section_text() {
+    $options = file_get_contents(TBE_PLUGIN_DIR."/data.json");
+    $options = $options ? @json_decode($options, true) : null;
+
+    //print_r(get_option(TBE_OPTIONS_NAME));
+
+    foreach($options as $key => $value) {
+        ?>
+        <div>
+            <label style="font-weight: bold"><?php echo esc_attr($key);?>:</label>
+            <textarea class="input-text" rows="3" name="translations[<?php echo esc_attr($key);?>]"><?php echo esc_attr($value);?></textarea>
+        </div>
+        <?php
+    }
+}
+
---
+++ text-blocks-editor.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ *
+"Login Redirect for WordPress" is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+"Login Redirect for WordPress" is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with "Login Redirect for WordPress". If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+ *
+ * Plugin Name: Text Blocks Editor
+ * Description: A plugin that allows you to use hardcoded texts and edit them via wp-admin
+ * Version: 1.0
+ * Author: Ivan Hanák <kontakt@hanakivan.sk>
+ * Author URI: https://hanakivan.sk
+ * Text Domain: tutor
+ * Requires at least: 5.0
+ * Requires PHP: 7.4
+ */
+
+const TBE_PLUGIN_NAME = "Text Blocks Editor";
+const TBE_SLUG = "text-blocks-editor";
+
+const TBE_OPTIONS_NAME = "tbe_plugin_options";
+define("TBE_PLUGIN_DIR", dirname(__FILE__));
+
+if(is_admin()) {
+	require_once TBE_PLUGIN_DIR."/settings-page.php";
+}
+
+function tbe_get_current_theme_textdomain(): string {
+	$td = (string)wp_get_theme()->get("TextDomain");
+
+	return $td ?: "default";
+}
+
+function tbe_get_translations(): array {
+	static $translations = null;
+
+	if($translations === null) {
+		$translations = get_option(TBE_OPTIONS_NAME);
+
+		if(!is_array($translations)) {
+			$translations = [];
+		}
+	}
+
+	return $translations;
+}
+
+add_filter("gettext_with_context_".tbe_get_current_theme_textdomain(), function ($translation, $text, $context, $domain) {
+	if($translation === $text && $context === $domain) {
+		if($context === tbe_get_current_theme_textdomain()) {
+			$translated = nl2br(tbe_get_translations()[$text]);
+
+			return $translated;
+		}
+	}
+
+	return $translation;
+}, 10, 4);
